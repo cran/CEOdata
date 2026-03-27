328713c1f69bee8580766ef6199cbb8649dbc375
Joel Ardiaca <joelardiacaarnau@gencat.cat>
2026-03-27 18:30:09 +0000

    version 1.4.0

--- inst/doc/using_CEOdata.R
+++ inst/doc/using_CEOdata.R
@@ -1,94 +1,147 @@
 ## ----echo=FALSE, message=FALSE, warning=FALSE---------------------------------
 library(CEOdata)
+library(tibble)
+library(dplyr)
+library(haven)
+library(stringr)
 
-## ----message = FALSE, echo = TRUE, eval = FALSE-------------------------------
-#  library(CEOdata)
-#  d <- CEOdata()
-
-## ----message = FALSE, echo = FALSE, eval = TRUE-------------------------------
-library(knitr)
-library(CEOdata)
-d <- CEOdata()
-# If there is an internet problem, do not run the remaining of the chunks.
-if (is.null(d)) {
-  print("here")
-  knitr::opts_chunk$set(eval = FALSE)
-} else {
-  knitr::opts_chunk$set(eval = TRUE)
+example_path <- function(filename) {
+  system.file("extdata", filename, package = "CEOdata", mustWork = TRUE)
 }
 
-## -----------------------------------------------------------------------------
-dim(d)
+acc_meta <- readRDS(example_path("accumulated_meta_example.rds"))
+meta <- readRDS(example_path("REO_meta_example.rds"))
 
-## -----------------------------------------------------------------------------
-d
+to_factor_tibble <- function(path) {
+  haven::read_sav(path) |>
+    tibble::as_tibble() |>
+    dplyr::mutate(
+      dplyr::across(
+        where(~ inherits(.x, "haven_labelled")),
+        haven::as_factor
+      )
+    )
+}
 
-## -----------------------------------------------------------------------------
-names(d)[1:50]
+to_raw_tibble <- function(path) {
+  haven::read_sav(path) |>
+    tibble::as_tibble()
+}
 
-## ---- eval = FALSE------------------------------------------------------------
-#  d.raw <- CEOdata(raw = FALSE)
+d <- to_factor_tibble(example_path("BOP_presencial_example.sav"))
+d_raw <- to_raw_tibble(example_path("BOP_presencial_example.sav"))
+d1145 <- to_factor_tibble(example_path("d1145_example.sav"))
+d1145_raw <- to_raw_tibble(example_path("d1145_example.sav"))
 
-## -----------------------------------------------------------------------------
-d746 <- CEOdata(reo = "746")
-d746
+CEOaccumulated_meta <- function(series = NULL, active_only = FALSE) {
+  out <- acc_meta
+  if (!is.null(series)) {
+    out <- out |>
+      dplyr::filter(codi_serie %in% series)
+  }
+  if (isTRUE(active_only) && "estat" %in% names(out)) {
+    estat_chr <- tolower(as.character(out$estat))
+    out <- out[stringr::str_detect(estat_chr, "\\bactiva\\b") %in% TRUE, , drop = FALSE]
+  }
+  out
+}
 
-## -----------------------------------------------------------------------------
-b2019 <- CEOdata(date_start = "2019-01-01", date_end = "2019-12-31")
-b2019
+CEOmeta <- function(reo = NULL, search = NULL, date_start = NA, date_end = NA, ...) {
+  out <- meta
+  if (!is.null(reo)) {
+    out <- out |>
+      dplyr::filter(as.character(REO) %in% as.character(reo))
+  } else if (!is.null(search)) {
+    cols <- intersect(c("Titol enquesta", "Titol estudi", "Objectius", "Resum", "Descriptors"), names(out))
+    if (length(cols) > 0) {
+      pattern <- paste(search, collapse = "|")
+      out <- out |>
+        dplyr::mutate(dplyr::across(dplyr::all_of(cols), ~ tolower(as.character(.x)))) |>
+        dplyr::filter(
+          dplyr::if_any(
+            dplyr::all_of(cols),
+            ~ stringr::str_detect(.x, regex(pattern, ignore_case = TRUE))
+          )
+        )
+    }
+  }
 
-## -----------------------------------------------------------------------------
-tail(names(d))
+  start <- suppressWarnings(as.Date(date_start))
+  end <- suppressWarnings(as.Date(date_end))
+  if (!is.na(start) && "Data d'alta al REO" %in% names(out)) {
+    out <- out |>
+      dplyr::filter(`Data d'alta al REO` >= start)
+  }
+  if (!is.na(end) && "Data d'alta al REO" %in% names(out)) {
+    out <- out |>
+      dplyr::filter(`Data d'alta al REO` <= end)
+  }
+  out
+}
 
-## -----------------------------------------------------------------------------
-d.lowercase <- d
-names(d.lowercase) <- tolower(names(d.lowercase))
+CEOdata <- function(series = "BOP_presencial", reo = NA, raw = FALSE) {
+  if (!is.na(reo)) {
+    return(if (isTRUE(raw)) d1145_raw else d1145)
+  }
+  if (!identical(series, "BOP_presencial")) {
+    stop(
+      "Offline vignette example includes only series = 'BOP_presencial'.",
+      call. = FALSE
+    )
+  }
+  if (isTRUE(raw)) d_raw else d
+}
 
-## -----------------------------------------------------------------------------
-CEOmeta()
+## ----echo=TRUE, message=FALSE, warning=FALSE----------------------------------
+head(CEOaccumulated_meta())
 
-## -----------------------------------------------------------------------------
-CEOmeta(reo = "746")
+## ----echo=TRUE, message=FALSE, warning=FALSE----------------------------------
+head(unique(CEOaccumulated_meta()$codi_serie))
 
-## -----------------------------------------------------------------------------
-CEOmeta(search = "Medi ambient")
+## ----echo=TRUE, message=FALSE, warning=FALSE----------------------------------
+head(CEOaccumulated_meta(series = "BOP_presencial"))
 
-## -----------------------------------------------------------------------------
-CEOmeta(search = c("Medi ambient", "Municipi"))
+## ----echo=TRUE, message=FALSE, warning=FALSE----------------------------------
+d <- CEOdata()
+head(d)
 
-## -----------------------------------------------------------------------------
-CEOmeta() |> 
-  filter(`Metode de recollida de dades` == "internet")
+## ----echo=TRUE, message=FALSE, warning=FALSE----------------------------------
+d <- CEOdata(series = "BOP_presencial")
+head(d)
 
-## -----------------------------------------------------------------------------
-CEOmeta() |>
-  filter(`Mostra estudis quantitatius` < 500)
+## ----echo=TRUE, message=FALSE, warning=FALSE----------------------------------
+try(CEOdata(series = "Longitudinal"))
 
-## -----------------------------------------------------------------------------
-CEOmeta(date_start = "2019-01-01", date_end = "2019-12-31")
+## ----echo=TRUE, message=FALSE, warning=FALSE----------------------------------
+d_raw <- CEOdata(series = "BOP_presencial", raw = TRUE)
+head(d_raw)
 
-## ---- eval = FALSE------------------------------------------------------------
-#  CEOmeta(search = "Medi ambient a", browse = TRUE)
+## ----echo=TRUE, message=FALSE, warning=FALSE----------------------------------
+meta <- CEOmeta()
+head(meta)
 
-## ---- eval = FALSE------------------------------------------------------------
-#  CEOmeta(reo = "746", browse = TRUE)
+## ----echo=TRUE, message=FALSE, warning=FALSE----------------------------------
+available <- CEOmeta() |> dplyr::filter(microdata_available)
+head(available)
 
-## ---- eval = FALSE------------------------------------------------------------
-#  CEOmeta(search = "Medi ambient a", browse = TRUE, browse_translate = "de")
+## ----echo=TRUE, message=FALSE, warning=FALSE----------------------------------
+specific_reo <- CEOmeta(reo = "1145")
+head(specific_reo)
 
-## -----------------------------------------------------------------------------
-CEOsearch(d)  # equivalent to CEOsearch(d, where = "variables")
+## ----echo=TRUE, message=FALSE, warning=FALSE----------------------------------
+d1145 <- CEOdata(reo = "1145")
+head(d1145)
 
-## -----------------------------------------------------------------------------
-CEOsearch(d, where = "values")
+## ----echo=TRUE, message=FALSE, warning=FALSE----------------------------------
+d1145_raw <- CEOdata(reo = "1145", raw = TRUE)
+head(d1145_raw)
 
-## -----------------------------------------------------------------------------
-CEOsearch(d, keyword = "edat")
+## ----echo=TRUE, message=FALSE, warning=FALSE----------------------------------
+head(CEOsearch(d1145, keyword = "democràcia"))
 
-## -----------------------------------------------------------------------------
-CEOsearch(d, keyword = "edat", translate = TRUE)
+## ----echo=TRUE, message=FALSE, warning=FALSE----------------------------------
+head(CEOsearch(d1145, keyword = "Catalunya", where = "values"))
 
-## -----------------------------------------------------------------------------
-CEOsearch(d) |>
-  left_join(CEOsearch(d, where = "values"))
+## ----echo=TRUE, message=FALSE, warning=FALSE----------------------------------
+packageVersion("CEOdata")
 
--- inst/doc/working_with_survey_data_using_the_CEOdata_package.R
+++ inst/doc/working_with_survey_data_using_the_CEOdata_package.R
@@ -1,43 +1,54 @@
 ## ----echo=FALSE, message=FALSE, warning=FALSE---------------------------------
 library(CEOdata)
-
-## ----message = FALSE, echo = TRUE, eval = FALSE-------------------------------
-#  library(CEOdata)
-#  d <- CEOdata()
-
-## ----message = FALSE, echo = FALSE, eval = TRUE-------------------------------
 library(knitr)
-library(CEOdata)
-d <- CEOdata()
-# If there is an internet problem, do not run the remaining of the chunks.
-if (is.null(d)) {
-  print("here")
-  knitr::opts_chunk$set(eval = FALSE)
-} else {
-  knitr::opts_chunk$set(eval = TRUE)
+library(tibble)
+library(dplyr)
+library(haven)
+
+example_path <- function(filename) {
+  system.file("extdata", filename, package = "CEOdata", mustWork = TRUE)
 }
 
-## ---- message = FALSE, warning = FALSE----------------------------------------
+d <- haven::read_sav(example_path("BOP_presencial_example.sav")) |>
+  tibble::as_tibble() |>
+  dplyr::mutate(
+    dplyr::across(
+      where(~ inherits(.x, "haven_labelled")),
+      haven::as_factor
+    )
+  )
+d_available <- all(c("SEXE", "BOP_NUM") %in% names(d))
+meta <- readRDS(example_path("REO_meta_example.rds"))
+meta_tags_available <- all(c("Descriptors", "REO") %in% names(meta))
+meta_fieldwork_available <- all(
+  c("Dia inici treball de camp", "Dia final treball de camp", "REO", "microdata_available") %in% names(meta)
+)
+
+## ----message = FALSE, echo = TRUE, eval = FALSE-------------------------------
+# library(CEOdata)
+# d <- haven::read_sav("../data/BOP_presencial_example.sav")
+
+## ----message = FALSE, warning = FALSE-----------------------------------------
 library(dplyr)
 library(tidyr)
 library(ggplot2)
 
-## -----------------------------------------------------------------------------
+## ----eval = d_available-------------------------------------------------------
 d |>
   count(SEXE)
 
-## ----prop-females, fig.width = 8, fig.height = 4, fig.cap = 'Proportion of females in the different Barometers.'----
+## ----prop-females, eval = d_available, fig.width = 8, fig.height = 4, fig.cap = 'Proportion of females in the different Barometers.'----
 d |>
   group_by(BOP_NUM) |>
-  summarize(propFemales = length(which(SEXE == "Dona")) / n()) |>
+  summarize(propFemales = length(which(SEXE == "Femení")) / n()) |>
   ggplot(aes(x = BOP_NUM, y = propFemales, group = 1)) +
   geom_point() +
   geom_line() +
   theme(axis.text.x = element_text(angle = 90, hjust = 1, vjust = 0.5)) +
   expand_limits(y = c(0, 1))
 
-## ----tags, fig.width = 6, fig.height = 6, fig.cap = 'Prevalence of topics covered.'----
-tags <- CEOmeta() |>
+## ----tags, eval = meta_tags_available, fig.width = 6, fig.height = 6, fig.cap = 'Prevalence of topics covered.'----
+tags <- meta |>
   separate_rows(Descriptors, sep = ";") |>
   mutate(tag = factor(stringr::str_trim(Descriptors))) |>
   select(REO, tag)
@@ -50,8 +61,8 @@ tags |>
     geom_point() +
     ylab("Topic")
 
-## ----fieldwork, fig.width = 8, fig.height = 10, fig.cap = 'Fieldwork periods.'----
-CEOmeta() |>
+## ----fieldwork, eval = meta_fieldwork_available, fig.width = 8, fig.height = 10, fig.cap = 'Fieldwork periods.'----
+meta |>
   filter(`Dia inici treball de camp` > "2018-01-01") |>
   ggplot(aes(xmin = `Dia inici treball de camp`,
              xmax = `Dia final treball de camp`,
@@ -61,7 +72,7 @@ CEOmeta() |>
   xlab("Date") + ylab("Surveys with fieldwork") +
   theme(axis.ticks.y = element_blank(), axis.text.y = element_blank())
 
-## -----------------------------------------------------------------------------
+## ----eval = d_available-------------------------------------------------------
 survey.data <- d |>
   mutate(Female = ifelse(SEXE == "Dona", 1, 0),
          Age = EDAT,
@@ -69,7 +80,7 @@ survey.data <- d |>
          Income = ifelse(INGRESSOS_1_15 %in% c("No ho sap", "No contesta"), 
                          NA,
                          INGRESSOS_1_15),
-         Date = Data,
+         Date = DATA_FIN,
          # Reorganize factor labels
          `Place of birth` = factor(case_when(
             LLOC_NAIX == "Catalunya" ~ "Catalonia",
@@ -97,28 +108,20 @@ survey.data <- d |>
 
 
 ## ----eval = FALSE-------------------------------------------------------------
-#  save(survey.data, file = "my_cleaned_dataset.RData")
+# save(survey.data, file = "my_cleaned_dataset.RData")
 
-## ---- eval = FALSE, echo = TRUE-----------------------------------------------
-#  library(vtable)
-#  st(survey.data)
+## ----eval = FALSE, echo = TRUE------------------------------------------------
+# library(vtable)
+# st(survey.data)
 
-## ---- eval = TRUE, echo = FALSE-----------------------------------------------
-if (exists("survey.data")) {
-  if (!is.null(survey.data)) {
-    vtable::st(survey.data, out = "kable")
-  }
-}
+## ----eval = exists("survey.data"), echo = FALSE-------------------------------
+vtable::st(survey.data, out = "kable")
 
-## ---- eval = FALSE, echo = TRUE-----------------------------------------------
-#  library(compareGroups)
-#  createTable(compareGroups(Female ~ . -Date, data = survey.data))
+## ----eval = FALSE, echo = TRUE------------------------------------------------
+# library(compareGroups)
+# createTable(compareGroups(Female ~ . -Date, data = survey.data))
 
-## ---- eval = TRUE, echo = FALSE-----------------------------------------------
-if (exists("survey.data")) {
-  if (!is.null(survey.data)) {
-    library(compareGroups)
-    createTable(compareGroups(Female ~ . -Date, data = survey.data))
-  }
-}
+## ----eval = exists("survey.data"), echo = FALSE-------------------------------
+library(compareGroups)
+createTable(compareGroups(Female ~ . -Date, data = survey.data))
 
